54b2d1fbeb22ca825ee54247f4d6f23bb2076984
Abdulhakeem Shaibu <hkmshb@gmail.com>
2020-03-03 22:03:50 +0100

    add more apps to brewfile and the python topic

---
+++ python/install.sh
@@ -0,0 +1,19 @@
+if [[ ! -d $(pyenv root)/plugins ]]; then
+  # create plugins folder
+  mkdir -p $(pyenv root)/plugins
+
+  # install python interpreter
+  pyenv install 3.8.1
+fi
+
+plugins=('virtualenv' 'update')
+for plugin in "${plugins[@]}"
+do
+  if [[ ! -d $(pyenv root)/plugins/pyenv-${plugin} ]]; then
+    echo "  Installing pyenv plugin: ${plugin} ..."
+    git clone https://github.com/pyenv/pyenv-${plugin}.git $(pyenv root)/plugins/pyenv-${plugin}
+  fi
+done
+
+# update plugins
+pyenv update
---
+++ python/pyenv.zsh
@@ -0,0 +1,6 @@
+# init
+if (( $+commands[pyenv] )); then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    pyenv global 3.8.1
+fi
