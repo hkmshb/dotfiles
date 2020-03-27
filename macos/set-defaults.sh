8fa16e69fbd606f06dd0348ecf9412781e4d438a
Abdulhakeem <hkmshb@gmaill.com>
2020-03-27 12:24:56 +0100

    add guards against running mac specific commands on linux

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,6 +7,7 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+if [[ "$(uname)" == "Darwin" ]]; then
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
@@ -39,3 +40,4 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+fi
