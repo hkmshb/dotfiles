b6160b569551af00727ffd73c3981d8f34d3feb7
Abdulhakeem Shaibu <hkmshb@gmail.com>
2021-02-25 14:11:34 +0100

    add more python aliases and update activated global interpreter

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,12 @@
 alias p-p='poetry publish --build -r pypi-test -u ${PYPI_USER}'
 alias p-pl='poetry publish --build -u ${PYPI_USER}'
-alias p-r='poetry run'
+
+alias pyenv-d='pyenv deactivate'
+alias pyenv-a='pyenv activate $*'
+
+alias pip='python -m pip'
+
+alias poet='pipx run poetry'
+alias poetry='pipx run poetry'
+alias precom='pipx run pre-commit'
+alias cookiecutter='pipx run cookiecutter'
