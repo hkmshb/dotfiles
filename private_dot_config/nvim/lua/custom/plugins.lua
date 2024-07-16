d60f03ff1d0d2daaec42b193d938c6e421bcf0c3
hkmshb <s.abdulhakeeem@gmail.com>
2024-07-16 17:02:37 +0100

    add neovim plugin for cold folding

--- private_dot_config/nvim/lua/custom/plugins.lua
+++ private_dot_config/nvim/lua/custom/plugins.lua
@@ -66,6 +66,61 @@ local plugins = {
     build = function() vim.fn["mkdp#util#install"]() end,
   },
 
+  {
+    "kevinhwang91/nvim-ufo",
+    event = "BufRead",
+    dependencies = {
+      { "kevinhwang91/promise-async" },
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            -- foldfunc = "builtin",
+            -- setopt = true,
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
+    },
+    keys = {
+      { "zR", function() require("ufo").openAllFolds() end },
+      { "zM", function() require("ufo").closeAllFolds() end },
+      { "zr", function() require("ufo").openFoldsExceptKinds() end },
+      { "zm", function() require("ufo").closeFoldsWith() end },
+      { "K", function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end }
+    },
+    config = function()
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:-,foldsep: ,foldclose:+]]
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      require("ufo").setup({
+        open_fold_hl_timeout = 150,
+        close_fold_kinds_for_ft = {
+          default = {'imports', 'comment'},
+          json = {'array'},
+          c = {'comment', 'region'}
+        },
+        provider_selector = function(bufnr, filetype, buftype)
+          return {'treesitter', 'indent'}
+        end
+      })
+    end,
+  }
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
